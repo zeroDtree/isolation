329e920cc759b2bdfc991e29522a2f76b7977b33
zengls@mac <3186428803@qq.com>
2026-04-20 14:23:58 +0800

    docker-ce-rootless-extras

--- docker/ubuntu/docker_rootless_prepare.sh
+++ docker/ubuntu/docker_rootless_prepare.sh
@@ -4,4 +4,4 @@ set -e
 sudo apt install -y uidmap
 sudo systemctl disable --now docker.service docker.socket
 sudo rm -f /var/run/docker.sock
-# sudo apt  -y install -y docker-ce-rootless-extras
+sudo apt install -y docker-ce-rootless-extras
